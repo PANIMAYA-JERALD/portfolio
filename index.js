1c166146d4386c3b4e396eb31c9789a0f454bedc
PANIMAYA-JERALD <panimayajerald@gmail.com>
2024-06-15 10:14:30 -0930

    portfolio

---
+++ index.js
@@ -0,0 +1,34 @@
+let ul =document.querySelector("ul")
+let box =document.querySelector(".box")
+ let resume =document.querySelector(".resume")
+ 
+ box.addEventListener('click' ,()=>{
+    ul.classList.toggle("showmenu")
+   
+ })
+ box.addEventListener('click' ,()=>{
+   resume.classList.toggle("resumee")
+  
+})
+//  let number = document.querySelector("#number")
+//  let counter =0;
+   // setInterval(() =>{
+      // if(counter ==85)
+// {
+   // clearInterval()
+// } else{
+//   counter += 1;
+//   number.innerHTML =counter +"%"     
+   // }
+// },50);
+//  let number1 = document.querySelector("#number")
+//  let counter1 =0;
+   // setInterval(() =>{
+      // if(counter ==85)
+// {
+   // clearInterval()
+// } else{
+//   counter += 1;
+//   number.innerHTML =counter1 +"%"     
+   // }
+// },50);
